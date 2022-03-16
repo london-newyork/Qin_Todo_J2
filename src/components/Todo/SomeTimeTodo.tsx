5122b9cbbb8ee5d04b74fff6297545fb68fa4a25
yamataso <yamataso0876@gmail.com>
2022-03-17 00:06:27 +0900

    複製アイコンと削除アイコンを追加しました

--- src/components/Todo/SomeTimeTodo.tsx
+++ src/components/Todo/SomeTimeTodo.tsx
@@ -1,18 +1,27 @@
+import { DocumentDuplicateIcon, TrashIcon } from "@heroicons/react/outline";
 import type { VFC } from "react";
 import { RadioBtn } from "src/components/btn/RadioBtn/RadioBtn";
 import { RadioBtnGroup } from "src/components/btn/RadioBtn/RadioBtnGroup";
-import { CopyDeleteIcon } from "src/components/CopyDeleteIcon";
 import { NewTask } from "src/components/NewTask";
 import { SomeTimeTitle } from "src/components/Title/SomeTimeTitle";
 
 export const SomeTimeTodo: VFC = () => {
+  const handleDuplicate = () => {
+    alert("Duplicate");
+  };
+  const handleTrash = () => {
+    alert("Trash");
+  };
   return (
     <div className="flex-1 w-full">
       <SomeTimeTitle />
       <RadioBtnGroup>
         <RadioBtn variant="yellow" value="task3">
-          Prettierをインストール
-          <CopyDeleteIcon />
+          <div className="group flex space-x-3">
+            <p>Prettierをインストール</p>
+            <DocumentDuplicateIcon onClick={handleDuplicate} className=" w-5 h-5 opacity-0 group-hover:opacity-100" />
+            <TrashIcon onClick={handleTrash} className="w-5 h-5 opacity-0 group-hover:opacity-100" />
+          </div>
         </RadioBtn>
       </RadioBtnGroup>
       <NewTask />
--- src/components/Todo/TomorrowTodo.tsx
+++ src/components/Todo/TomorrowTodo.tsx
@@ -1,18 +1,27 @@
+import { DocumentDuplicateIcon, TrashIcon } from "@heroicons/react/outline";
 import type { VFC } from "react";
 import { RadioBtn } from "src/components/btn/RadioBtn/RadioBtn";
 import { RadioBtnGroup } from "src/components/btn/RadioBtn/RadioBtnGroup";
-import { CopyDeleteIcon } from "src/components/CopyDeleteIcon";
 import { NewTask } from "src/components/NewTask";
 import { TomorrowTitle } from "src/components/Title/TomorrowTitle";
 
 export const TomorrowTodo: VFC = () => {
+  const handleDuplicate = () => {
+    alert("Duplicate");
+  };
+  const handleTrash = () => {
+    alert("Trash");
+  };
   return (
     <div className="flex-1 w-full">
       <TomorrowTitle />
       <RadioBtnGroup>
         <RadioBtn variant="orange" value="task2">
-          ESLintをインストール
-          <CopyDeleteIcon />
+          <div className="group flex space-x-3">
+            <p>ESLintをインストール</p>
+            <DocumentDuplicateIcon onClick={handleDuplicate} className=" w-5 h-5 opacity-0 group-hover:opacity-100" />
+            <TrashIcon onClick={handleTrash} className="w-5 h-5 opacity-0 group-hover:opacity-100" />
+          </div>
         </RadioBtn>
       </RadioBtnGroup>
       <NewTask />
